603ef0f9df5e9d9d3b35f529a05da58945bdd09a
ederp <eder@infotera.com.br>
2024-10-16 08:41:48 -0300

    changes

--- src/main/java/br/com/infotravel/api/commonv1/TestServico.java
+++ src/main/java/br/com/infotravel/api/commonv1/TestServico.java
@@ -1,12 +1,28 @@
 package br.com.infotravel.api.commonv1;
 
 import br.com.infotravel.api.commonv1.client.InfotravelClient;
+import br.com.infotravel.api.commonv1.dto.ApiBooking;
+import br.com.infotravel.api.commonv1.dto.ApiContact;
+import br.com.infotravel.api.commonv1.dto.ApiName;
 import br.com.infotravel.api.commonv1.dto.ApiToken;
+import br.com.infotravel.api.commonv1.dto.ticket.ApiBookingTicket;
+import br.com.infotravel.api.commonv1.dto.ticket.ApiTicket;
+import br.com.infotravel.api.commonv1.dto.ticket.ApiTicketAvail;
+import br.com.infotravel.api.commonv1.dto.ticket.ApiTicketDate;
+import br.com.infotravel.api.commonv1.dto.ticket.ApiTicketModalitie;
+import br.com.infotravel.api.commonv1.dto.tour.ApiBookingTour;
+import br.com.infotravel.api.commonv1.dto.tour.ApiTour;
+import br.com.infotravel.api.commonv1.dto.tour.ApiTourAvail;
+import br.com.infotravel.api.commonv1.exceptions.ApiException;
 import br.com.infotravel.api.commonv1.requests.ActivityAvailabilityRQ;
 import br.com.infotravel.api.commonv1.requests.AuthenticationRQ;
 import br.com.infotravel.api.commonv1.requests.BookingRQ;
 import br.com.infotravel.api.commonv1.responses.BookingRS;
 import br.com.infotravel.api.commonv1.responses.TourAvailbilityRS;
+import br.com.infotravel.api.commonv1.utils.Utils;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -23,9 +39,10 @@ public class TestServico {
     private static final String baseUrl = "http://develop.dev-infotravel.com.br/infotravel/api/v1";
     private static InfotravelClient infotravelClient = new InfotravelClient(baseUrl);
 
-    public static void main(String[] args) {
-//        try {
 
+
+
+    public static void main(String[] args) {
         AuthenticationRQ authenticationRQ = new AuthenticationRQ("parceirows", "parceiro123", "2", "DEVELOP", "pt_br");
 
         ApiToken token = infotravelClient.authenticate(authenticationRQ);
@@ -34,29 +51,74 @@ public class TestServico {
         ActivityAvailabilityRQ availabilityRQ = new ActivityAvailabilityRQ("2024-11-10", "2024-11-20", "2", "2802");
         TourAvailbilityRS availbilityRS = infotravelClient.tourAvail(availabilityRQ, token);
 
-        System.out.println(availbilityRS);
+        if (availbilityRS != null && !availbilityRS.getTourAvailList().isEmpty()) {
 
-//        } catch (IOException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
+            ApiBooking requestCheckRate = montaCheckRate(availbilityRS.getTourAvailList().get(0));
+            ApiBooking responseCheckRate = chamaCheckRate(requestCheckRate, token);
+
+            if (responseCheckRate.getBookingTours() != null && !responseCheckRate.getBookingTours().isEmpty()) {
+                ApiBooking requestBooking = montaBooking(responseCheckRate);
+                ApiBooking responseBooking = chamaBooking(requestBooking, token);
+                System.out.println("BOOKING CREATE '" + responseBooking.getId() + "' STATUS '" + responseBooking.getStatus().name() + "' ");
+            }
+
+        }
+
     }
 
-    public static void checkRate() {
 
-//        try {
-        AuthenticationRQ authenticationRQ = new AuthenticationRQ("123", "123", "2", "DEVELOP", "pt_br");
-
-        ApiToken token = infotravelClient.authenticate(authenticationRQ);
-        System.out.println("Token obtido: " + token.getAccessToken());
-
-        BookingRQ rq = new BookingRQ();
-        BookingRS bookingRS = infotravelClient.checkRate(rq, token);
-
-        System.out.println(bookingRS);
-
-//        } catch (IOException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
+    public static ApiBooking chamaCheckRate(ApiBooking booking, ApiToken token) throws ApiException {
+        BookingRS rs = infotravelClient.checkRate(new BookingRQ(booking), token);
+        return rs.getBooking();
     }
+
+    public static ApiBooking chamaBooking(ApiBooking booking, ApiToken token) throws ApiException {
+        BookingRS rs = infotravelClient.booking(new BookingRQ(booking), token);
+
+        return rs.getBooking();
+    }
+
+    public static List<ApiName> montaNomes(List<ApiName> names) {
+        ApiName name = names.get(0);
+        name.setFirstName("JOAO");
+        name.setLastName("Gomes");
+        name.setBirth(Utils.toDate("1987-12-01", "yyyy-MM-dd"));
+        name.setAge(36);
+
+        name = names.get(1);
+        name.setFirstName("MARIA");
+        name.setLastName("Gomes");
+        name.setBirth(Utils.toDate("1987-12-01", "yyyy-MM-dd"));
+        name.setAge(36);
+        return names;
+    }
+
+    public static ApiBooking montaCheckRate(ApiTourAvail tourAvail) {
+        ApiBooking booking = new ApiBooking();
+        if (tourAvail != null) {
+            ApiBookingTour apiBookingTour = new ApiBookingTour(new ApiTour(tourAvail.getTour().getKey()), tourAvail.getDates().get(0), tourAvail.getNames());
+            booking.setBookingTours(List.of(apiBookingTour));
+        }
+        return booking;
+    }
+
+
+    public static ApiBooking montaBooking(ApiBooking bookingCheckRate) {
+        ApiBooking booking = new ApiBooking();
+        booking.setContact(new ApiContact("Joao Teste", "teste@infotera.com.br", "213213123213"));
+
+        if (!bookingCheckRate.getBookingTours().isEmpty()) {
+
+            ApiBookingTour bookingTourCheckRate = bookingCheckRate.getBookingTours().get(0);
+
+
+            ApiBookingTour bookingTour = new ApiBookingTour(new ApiTour(bookingTourCheckRate.getTour().getKey()), bookingTourCheckRate.getTour().getDate(), montaNomes(bookingTourCheckRate.getNames()));
+
+            booking.setBookingTours(List.of(bookingTour));
+        }
+
+        return booking;
+    }
+
 
 }
